0529d18500d50438c13a7c711c3d9d7024a449e1
Samir Sen <samirsen@stanford.edu>
2019-06-08 23:59:17 -0700

    finished resnet model

--- model/resnet34.py
+++ model/resnet34.py
@@ -14,8 +14,33 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
-def build_skip_connectors(block):
-    pass 
+def build_skip_connectors(in_channels=64, out_channels=128, layers = [3, 4, 6, 3]):
+    skip_layers = []
+
+    downsample = None
+    stride = 1
+    for i, num_blocks in enumerate(layers):
+        next_in_channels = out_channels
+        next_out_channels = out_channels * 2
+
+        if i > 0:
+            downsample = nn.Sequential(
+                conv1x1(in_channels, out_channels, stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+        curr_block_layers = []
+        curr_block_layers.append(ResBlock(64, out_channels, stride, downsample))
+
+        for _ in range(1, num_blocks):
+            curr_block_layers.append(block(out_channels, out_channels))
+
+        skip_layers += curr_block_layers
+        in_channels = next_in_channels
+        out_channels = next_out_channels
+
+    return nn.Sequential(*skip_layers)
+
 
 def build_featurizer(in_channel, out_channel):
     return [
@@ -26,6 +51,19 @@ def build_featurizer(in_channel, out_channel):
         nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
     ]
 
+def build_base_resnet(in_channels=3, out_channels=64, num_classes=100, perturb=False):
+    res_layers = []
+    res_layers += build_featurizer(in_channels, out_channels)
+    res_layers += build_skip_connectors(out_channels, out_channels*2)
+    res_layers += [nn.AdaptiveAvgPool2d((1, 1))]
+    res_layers += [nn.Linear(512, num_classes)]
+
+    if perturb:
+        pass
+
+    return nn.Sequential(*res_layers)
+
+
 def build_res_block(in_channels, out_channels):
     return nn.Sequential(
         conv3x3(in_channels, out_channels),
@@ -35,25 +73,11 @@ def build_res_block(in_channels, out_channels):
         nn.BatchNorm2d(out_channels)
     )
 
-
-def build_base_resnet(in_channels, out_channels, block, num_classes=100, perturb=False):
-    res_layers = []
-    res_layers += build_featurizer(3, 64)
-    res_layers += build_skip_connectors(block)
-    res_layers += [nn.AdaptiveAvgPool2d((1, 1))]
-    res_layers += [nn.Linear(512 * block.expansion, num_classes)]
-
-    if perturb:
-        pass
-
-    return nn.Sequential(*res_layers)
-
-
 class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, expansion=1, downsample=None):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         Super(ResBlock, self).__init__()
 
-        self.expansion = expansion
+        self.expansion = 1
         self.downsample = downsample
         self.res_block = build_res_block(in_channels, out_channels)
 
--- train.py
+++ train.py
@@ -1,2 +1,10 @@
+# Investigating random perturbation effects in serial and residual networks
+# What we find is that serial networks performance fluctuates with more
+# volatility in comparison with residual networks with skip connections.
+#
+# TODO: Add visualization for final layer activations.
+#
+# Author: Samir Sen, 2019 
+
 import torch
-from utils import * 
+from utils import *
