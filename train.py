39fe457ceeb064b5ee28c5bcebe275787eea8f65
Samir Sen <samirsen@stanford.edu>
2019-06-09 03:54:38 -0700

    uploading

--- train.py
+++ train.py
@@ -4,7 +4,43 @@
 #
 # TODO: Add visualization for final layer activations.
 #
-# Author: Samir Sen, 2019 
+# Author: Samir Sen, 2019
 
+import os
 import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.utils.data as data
+import torchvision
+from torchvision import datasets, models, transforms
+
+import numpy as np
 from utils import *
+from model.vgg19 import VGG, FullyConnected
+from model.resnet34 import Resnet
+
+
+save_path = 'checkpoints/'
+use_cuda = torch.cuda.is_available()
+
+best_acc = 0
+num_classes = 100
+
+vgg = VGG()
+vgg_model = load_model(vgg, 'vgg')
+if use_cuda: vgg_model = vgg_model.cuda()
+
+print('   Total params: %.2fM' % (num_params(vgg)))
+
+for i, param in vgg_model.named_parameters():
+    param.requires_grad = False
+
+vgg_model.classifier = FullyConnected(512 * 7 * 7, 4096, num_classes)
+
+print_params(vgg_model)
+
+criterion = nn.CrossEntropyLoss()
+trainable_params = list(filter(lambda p: p.requires_grad, vgg_model.parameters()))
+optimizer = optim.SGD(trainable_params, lr=0.001, momentum=0.9)
--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
+import os
+import sys
 import torch
+import torchvision
+import torch.utils.data as data
+import torch.nn.init as init
+from torch.autograd import Variable
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
@@ -9,11 +17,160 @@ pretrained_weights = {
     'resnet': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth'
 }
 
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+def num_params(model):
+    return sum([p.numel() for p in model.parameters()])/1000000.0
+
+
+def print_params(model):
+    for name, child in model.named_childeren():
+      for name_2, params in child.named_parameters():
+        print(name_2, params.requires_grad)
+
+def accuracy(output, target, topk=(1,)):
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
 def load_model(model, arch='vgg'):
     state_dict = load_state_dict_from_url(pretrained_weights[arch], progress=True)
     model.load_state_dict(state_dict)
 
     return model
 
-def load_data():
-    pass 
+def load_data(Train=True, batch_size=128, download=True):
+    transform_fn = transform_train if Train else transform_test
+    dataset = datasets.CIFAR100(root='./data', train=Train, download=download, transform=transform_fn)
+    loader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return dataset, loader
+
+def savefig(fname, dpi=None):
+    dpi = 150 if dpi == None else dpi
+    plt.savefig(fname, dpi=dpi)
+
+def plot_overlap(logger, names=None):
+    names = logger.names if names == None else names
+    numbers = logger.numbers
+    for _, name in enumerate(names):
+        x = np.arange(len(numbers[name]))
+        plt.plot(x, np.asarray(numbers[name]))
+    return [logger.title + '(' + name + ')' for name in names]
+
+class AverageMeter(object):
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class Logger(object):
+    '''Save training process to log file with simple plot function.'''
+    def __init__(self, fpath, title=None, resume=False):
+        self.file = None
+        self.resume = resume
+        self.title = '' if title == None else title
+        if fpath is not None:
+            if resume:
+                self.file = open(fpath, 'r')
+                name = self.file.readline()
+                self.names = name.rstrip().split('\t')
+                self.numbers = {}
+                for _, name in enumerate(self.names):
+                    self.numbers[name] = []
+
+                for numbers in self.file:
+                    numbers = numbers.rstrip().split('\t')
+                    for i in range(0, len(numbers)):
+                        self.numbers[self.names[i]].append(numbers[i])
+                self.file.close()
+                self.file = open(fpath, 'a')
+            else:
+                self.file = open(fpath, 'w')
+
+    def set_names(self, names):
+        if self.resume:
+            pass
+        # initialize numbers as empty list
+        self.numbers = {}
+        self.names = names
+        for _, name in enumerate(self.names):
+            self.file.write(name)
+            self.file.write('\t')
+            self.numbers[name] = []
+        self.file.write('\n')
+        self.file.flush()
+
+
+    def append(self, numbers):
+        assert len(self.names) == len(numbers), 'Numbers do not match names'
+        for index, num in enumerate(numbers):
+            self.file.write("{0:.6f}".format(num))
+            self.file.write('\t')
+            self.numbers[self.names[index]].append(num)
+        self.file.write('\n')
+        self.file.flush()
+
+    def plot(self, names=None):
+        names = self.names if names == None else names
+        numbers = self.numbers
+        for _, name in enumerate(names):
+            x = np.arange(len(numbers[name]))
+            plt.plot(x, np.asarray(numbers[name]))
+        plt.legend([self.title + '(' + name + ')' for name in names])
+        plt.grid(True)
+
+    def close(self):
+        if self.file is not None:
+            self.file.close()
+
+class LoggerMonitor(object):
+    '''Load and visualize multiple logs.'''
+    def __init__ (self, paths):
+        '''paths is a distionary with {name:filepath} pair'''
+        self.loggers = []
+        for title, path in paths.items():
+            logger = Logger(path, title=title, resume=True)
+            self.loggers.append(logger)
+
+    def plot(self, names=None):
+        plt.figure()
+        plt.subplot(121)
+        legend_text = []
+        for logger in self.loggers:
+            legend_text += plot_overlap(logger, names)
+        plt.legend(legend_text, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.grid(True)
